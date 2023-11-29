fc892c25e135b22f3299b547142bb067d68350c6
Andres Agudelo <andyagudelo42@gmail.com>
2023-11-28 20:04:52 -0500

    Added some elements
    Broken light/dark mode
    Expander
    Another line chart using radio button for EMA/SMA

--- FinalProject/FinalProject.py
+++ FinalProject/FinalProject.py
@@ -35,12 +35,43 @@ def fetch_stock_data(symbol):
             return None
         else:
             return None
+st.set_page_config(
+    page_title="Financity"
+)
+        
+if 'mode' not in st.session_state:
+    st.session_state['mode'] = 'Light'
+
+if st.button(f'Switch to {"Dark" if st.session_state.mode == "Light" else "Light"} Mode'):
+    st.session_state.mode = 'Dark' if st.session_state.mode == 'Light' else 'Light'
+
+# Apply styles based on the mode
+if st.session_state.mode == 'Dark':
+    # Apply the dark theme
+    st.markdown("""
+        <style>
+        .main {
+            textColor: white;
+            background-color: #000000;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+elif st.session_state.mode == 'Light':
+    # Apply the light theme with black text
+    st.markdown("""
+        <style>
+        .main {
+            textColor: #000000;
+            background-color: #FFF;
+        }
+        </style>
+        """, unsafe_allow_html=True)
 
 col1, col2= st.columns(2)
+
 with col1:
     st.title("financity")
     st.subheader("all your financial data in one place.")
-    st.button('Get Started', type="primary")
     
     # User inputs the stock symbol
     symbol = st.text_input('Ticker Symbol', '')
@@ -92,3 +123,43 @@ if symbol:
         
     else:
         st.warning("No historical stock data found for the given symbol.")
+
+def loadSMA():
+    url = f'https://www.alphavantage.co/query?function=SMA&symbol=IBM&interval=weekly&time_period=10&series_type=open&apikey={api_key}'
+    r = requests.get(url)
+
+    data = r.json()
+
+    # Extract data
+    sma_data = data['Technical Analysis: SMA']
+    df = pd.DataFrame(sma_data).transpose()
+    df.index = pd.to_datetime(df.index)
+    df['SMA'] = df['SMA'].astype(float)
+    return df
+
+def loadEMA():
+    url = f'https://www.alphavantage.co/query?function=EMA&symbol=IBM&interval=weekly&time_period=10&series_type=open&apikey={api_key}'
+    r = requests.get(url)
+
+    data = r.json()
+
+    # Extract data
+    ema_data = data['Technical Analysis: EMA']
+    df = pd.DataFrame(ema_data).transpose()
+    df.index = pd.to_datetime(df.index)
+    df['EMA'] = df['EMA'].astype(float)
+    return df
+
+option = st.radio('View technical indicators', ['SMA', 'EMA'])
+
+if option is 'SMA':
+    df_sma = loadSMA()
+    st.line_chart(df_sma['SMA'])
+    with st.expander("What is the SMA?"):
+        st.write("The Simple Moving Average (SMA) is a widely used technical indicator in financial analysis, particularly in the analysis of stock and other asset prices.")
+
+else:
+    df_ema = loadEMA()
+    st.line_chart(df_ema['EMA'])
+    with st.expander("What is the EMA?"):
+        st.write("The Exponential Moving Average (EMA) is another key technical indicator used in financial markets, particularly in the analysis of stock, forex, and other tradable assets' prices.")
