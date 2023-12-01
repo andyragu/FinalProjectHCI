5e69f069d1a5e9292fc2b9a547560c06c460069f
andysantanar <andysantana00@gmail.com>
2023-12-01 17:39:19 -0500

    Reload SMA EMA

--- FinalProject/FinalProject.py
+++ FinalProject/FinalProject.py
@@ -136,3 +136,111 @@ if symbol and start_date and end_date:
 
         else:
             st.warning("No historical stock data found for the given symbol.")
+
+def loadSMA():
+    url = f'https://www.alphavantage.co/query?function=SMA&symbol={symbol}&interval=weekly&time_period=10&series_type=open&apikey={api_key}'
+    r = requests.get(url)
+
+    data = r.json()
+
+    # Extract data
+    sma_data = data['Technical Analysis: SMA']
+    df = pd.DataFrame(sma_data).transpose()
+    df.index = pd.to_datetime(df.index)
+    df['SMA'] = df['SMA'].astype(float)
+    return df
+
+def loadEMA():
+    url = f'https://www.alphavantage.co/query?function=EMA&symbol={symbol}&interval=weekly&time_period=10&series_type=open&apikey={api_key}'
+    r = requests.get(url)
+
+    data = r.json()
+
+    # Extract data
+    ema_data = data['Technical Analysis: EMA']
+    df = pd.DataFrame(ema_data).transpose()
+    df.index = pd.to_datetime(df.index)
+    df['EMA'] = df['EMA'].astype(float)
+    return df
+
+
+if symbol:
+    # Display the option to select technical indicators
+    option = st.radio('View technical indicators', ['SMA', 'EMA', 'Disabled'])
+
+    if option == 'SMA':
+        df_sma = loadSMA()
+        st.line_chart(df_sma['SMA'])
+        with st.expander("What is the SMA?"):
+            st.write("The Simple Moving Average (SMA) is a widely used technical indicator in financial analysis, particularly in the analysis of stock and other asset prices.")
+
+    elif option == 'EMA':
+        df_ema = loadEMA()
+        st.line_chart(df_ema['EMA'])
+        with st.expander("What is the EMA?"):
+            st.write("The Exponential Moving Average (EMA) is another key technical indicator used in financial markets, particularly in the analysis of stock, forex, and other tradable assets' prices.")
+
+st.divider()
+st.subheader("Earnings Calendar")
+#THIS IS BROKEN, If you wanna try and fix go ahead if not just remove
+# if 'tickers' not in st.session_state:
+#     st.session_state['tickers'] = []
+
+# # Text input for the ticker symbol
+# ticker_symbol = st.text_input("Enter the ticker symbol")
+
+# # Button to add the ticker to the list
+# if st.button("Add Ticker"):
+#     if ticker_symbol:
+#         if ticker_symbol not in st.session_state['tickers']:
+#             st.session_state['tickers'].append(ticker_symbol)
+#             st.success(f"Ticker {ticker_symbol} added!")
+#         else:
+#             st.warning(f"Ticker {ticker_symbol} is already in the list.")
+#     else:
+#         st.error("Please enter a ticker symbol.")
+
+# Concatenate ticker symbols into a string
+# tickers_str = ','.join(st.session_state['tickers'])
+
+# def advancedAnalytics(start_date, end_date):
+#     url = f'https://alphavantageapi.co/timeseries/analytics?SYMBOLS={tickers_str}&RANGE={start_date}&RANGE={end_date}&INTERVAL=DAILY&OHLC=close&CALCULATIONS=MEAN,STDDEV,CORRELATION&apikey={api_key}'
+#     r = requests.get(url)
+#     data = r.json()
+
+#     if 'payload' in data and 'RETURNS_CALCULATIONS' in data['payload']:
+#         symbolAnalytics = data['payload']['RETURNS_CALCULATIONS']
+#         df = pd.DataFrame(symbolAnalytics)
+#         return df
+#     else:
+#         return pd.DataFrame()  # Return an empty DataFrame if no data
+
+# # Date range input
+# d = st.date_input("Select the range in which you want to see the company's analytics", value=(date.today(), date.today()), format="YYYY-MM-DD")
+
+# start_date, end_date = d
+
+# if start_date and end_date:
+#     analytics_df = advancedAnalytics(start_date, end_date)
+#     if not analytics_df.empty:
+#         st.table(analytics_df)
+#     else:
+#         st.error("No analytics data found for the selected date range.")
+# else:
+#     st.error("Please select a valid date range.")
+#End of broken code
+
+horizon = st.selectbox('What horizon would you like to see?', ('3month', '6month', '12month'))
+
+def loadEarningsCalendar():
+    url = f'https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&symbol={symbol}&horizon={horizon}&apikey={api_key}'
+
+    with requests.Session() as s:
+        download = s.get(url)
+        decoded_content = download.content.decode('utf-8')
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        my_list = list(cr)
+        df = pd.DataFrame(my_list[1:], columns=my_list[0])  # Assuming first row is header
+        st.dataframe(df)
+if symbol:
+    loadEarningsCalendar()
